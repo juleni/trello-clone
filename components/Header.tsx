0520f3aa9352ff9e83ca44d96daae16b2f139b6b
juleni <69912179+juleni@users.noreply.github.com>
2023-06-07 17:20:37 +0200

    Added Header component

---
+++ components/Header.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Avatar from "react-avatar";
+
+function Header() {
+  return (
+    <header>
+      <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/en/8/8c/Trello_logo.svg"
+          alt="Trello Image"
+          width={300}
+          height={100}
+          className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+        />
+        <div className="flex items-center space-x-5 flex-1 justify-end">
+          {/** Search box */}
+          <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+            <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search"
+              className="flex-1 outline-none p-2"
+            />
+            <button type="submit" hidden>
+              Search
+            </button>
+          </form>
+
+          {/** Avatar */}
+          <Avatar name="Juleni" round color="#0055d1" size="50" />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
