259da082c335a4c2973fdb669c4143c5656043c6
juleni <69912179+juleni@users.noreply.github.com>
2023-06-08 08:19:17 +0200

    Added input for chatbot

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Avatar from "react-avatar";
 
@@ -8,6 +8,7 @@ function Header() {
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-500 to-[#0055d1] rounded-md filter blur-3xl opacity-50 -z-50"></div>
         <Image
           src="https://upload.wikimedia.org/wikipedia/en/8/8c/Trello_logo.svg"
           alt="Trello Image"
@@ -15,7 +16,7 @@ function Header() {
           height={100}
           className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
         />
-        <div className="flex items-center space-x-5 flex-1 justify-end">
+        <div className="flex items-center space-x-5 flex-1 justify-end w-full">
           {/** Search box */}
           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
@@ -32,6 +33,12 @@ function Header() {
           {/** Avatar */}
           <Avatar name="Juleni" round color="#0055d1" size="50" />
         </div>
+      </div>
+      <div className="flex items-center justify-center px-5 py-2 md:py-5">
+        <p className="flex items-center text-center font-light pr-5 p-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055d1]">
+          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055d1] mr-1" />
+          GPT is sumarising your tasks for the day...
+        </p>
       </div>
     </header>
   );
