7e82b2038d1142a353c226598d40fcad433302e1
juleni <69912179+juleni@users.noreply.github.com>
2023-06-09 14:58:28 +0200

    Added deleteTask functionality

--- store/BoardStore.tsx
+++ store/BoardStore.tsx
@@ -1,4 +1,4 @@
-import { databases } from "@/appwrite";
+import { databases, storage } from "@/appwrite";
 import { getTodosGroupedByColumns } from "@/lib/getTodosGroupedByColumns";
 import { create } from "zustand";
 
@@ -10,9 +10,11 @@ interface BoardState {
 
   searchString: string;
   setSearchString: (searchString: string) => void;
+
+  deleteTask: (taskIndex: number, todoId: Todo, id: TypedColumn) => void;
 }
 
-export const useBoardStore = create<BoardState>((set) => ({
+export const useBoardStore = create<BoardState>((set, get) => ({
   board: { columns: new Map<TypedColumn, Column>() },
   searchString: "",
 
@@ -24,6 +26,25 @@ export const useBoardStore = create<BoardState>((set) => ({
   },
 
   setBoardState: (board) => set({ board }),
+
+  deleteTask: async (taskIndex: number, todo: Todo, id: TypedColumn) => {
+    const newColumns = new Map(get().board.columns);
+    // Delete todoId from newColumns
+    newColumns.get(id)?.todos.splice(taskIndex, 1);
+
+    set({ board: { columns: newColumns } });
+
+    // Delete file in AppWrite storage bucket
+    if (todo.image) {
+      await storage.deleteFile(todo.image.bucketId, todo.image.fileId);
+    }
+    // Delete document from DB
+    await databases.deleteDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id
+    );
+  },
 
   updateTodoInDB: async (todo, columnId) => {
     await databases.updateDocument(
