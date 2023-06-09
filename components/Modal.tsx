9482ee70da0c25bb138ff78310daa7cd93efa936
juleni <69912179+juleni@users.noreply.github.com>
2023-06-09 16:07:25 +0200

    Added divs to 2nd transition child /changed styles

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -15,8 +15,8 @@ function Modal() {
 
   return (
     // Use the `Transition` component at the root level
-    <Transition show={isOpen} as={Fragment}>
-      <Dialog onClose={closeModal}>
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="form" className="relative z-10" onClose={closeModal}>
         {/*
           Use one Transition.Child to apply one transition to the backdrop...
         */}
@@ -29,28 +29,36 @@ function Modal() {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black/30" />
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
         </Transition.Child>
-
         {/*
           ...and another Transition.Child to apply a separate transition
           to the contents.
         */}
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <Dialog.Panel>
-            <Dialog.Title>Deactivate account</Dialog.Title>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="fixed inset-0 flex items-center justify-center p-4">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900 pb-2"
+                >
+                  Add a Task
+                </Dialog.Title>
 
-            {/* ... */}
-          </Dialog.Panel>
-        </Transition.Child>
+                {/* ... */}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
       </Dialog>
     </Transition>
   );
